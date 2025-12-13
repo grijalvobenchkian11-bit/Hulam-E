12b2006c1ddceceb32f292efd0384f813b33aeb5
grijalvobenchkian11-bit <grijalvo.benchkian11@gmail.com>
2025-12-14 05:35:39 +0800

    Update api.php

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,4 +1,8 @@
-<?php
+use Illuminate\Http\Request;
+
+Route::options('/{any}', function () {
+    return response()->json([], 200);
+})->where('any', '.*');
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
